f64a9da3aef4a426210e06a801b149eb884c548b
B12_309_21\b12s309 <mlujan.2325@gmail.com>
2024-08-22 11:12:46 -0500

    create method add tag

--- todo/model/todo.py
+++ todo/model/todo.py
@@ -10,6 +10,12 @@ class Todo:
     def mark_completed(self):
         self.completed = True
 
+    def add_tag(self, tag: str):
+        if tag not in self.tags:
+            self.tags.append(tag)
+
+
+
 
 
 
